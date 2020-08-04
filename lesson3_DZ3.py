9622f7e0a7b98a7e67a0daacba40996f39524f3d
Admin <aleksandrbelokrys@gmail.com>
2020-08-04 20:53:29 +0300

    Create new file:lesson3_DZ3.py

---
+++ lesson3_DZ3.py
@@ -0,0 +1,16 @@
+#Задание 1
+my_str = "blablacar"
+my_symbol = "bla"
+for my_symbol in my_str[:6]:
+    print(my_symbol)
+
+
+# Задание 2
+my_str = "blablacar"
+my_symbol = "bla"
+print(my_str.count(my_symbol))
+##################################################
+
+# Задание 3
+my_str="bla BLA car"
+print(len(set(my_str.lower())))
