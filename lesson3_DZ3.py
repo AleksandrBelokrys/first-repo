1709f3f9190d605c95ab92c072ab11793759460b
Admin <aleksandrbelokrys@gmail.com>
2020-08-04 21:20:27 +0300

    Create new file:lesson3_DZ3.py

--- lesson3_DZ3.py
+++ lesson3_DZ3.py
@@ -1,15 +1,16 @@
 #Задание 1
 my_str = "blablacar"
 my_symbol = "bla"
+print(my_symbol * my_str.count(my_symbol))
 
 
 
 # Задание 2
-my_str = "blablacar"
-my_symbol = "bla"
-print(my_str.count(my_symbol))
+# my_str = "blablacar"
+# my_symbol = "bla"
+# print(my_str.count(my_symbol))
 ##################################################
 
 # Задание 3
-my_str="bla BLA car"
-print(len(set(my_str.lower())))
+# my_str="bla BLA car"
+# print(len(set(my_str.lower())))
