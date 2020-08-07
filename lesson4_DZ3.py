376dec9bef49682f9764b16ea1f1ae3e41fb672e
Admin <aleksandrbelokrys@gmail.com>
2020-08-07 13:28:58 +0300

    Create new file:lesson4_DZ3.py

---
+++ lesson4_DZ3.py
@@ -0,0 +1,29 @@
+#Задание 1
+
+# my_indexes = [0, 1, 2, 3, 4, 5]
+# my_list = "qwerty"
+# for index in my_indexes:
+#     print(my_list[index])
+
+###########################
+#Задача 2
+# my_list_1 = [0, 1, 2, 3]
+# my_list_2 = [0, 1, 2, 3]
+# my_indexes = list(zip(my_list_1, my_list_2))
+# for index in my_indexes:
+#     print(index)
+
+###############################################
+#Задача 3
+# my_string = "0123456789"
+# my_list = []
+# for symb_1 in my_string:
+#     for symb_2 in my_string:
+#         my_list.append(symb_1+symb_2)
+# my_list_2 = [int(x) for x in my_list ]
+# print(my_list_2)
+
+
+
+
+
