1f6eb19f947bcf21e957fc7b518a6fc5f22e6e74
Admin <aleksandrbelokrys@gmail.com>
2020-08-11 22:30:05 +0300

    Create new file:lesson5_DZ5.py

--- lesson5_DZ5.py
+++ lesson5_DZ5.py
@@ -2,33 +2,35 @@
 
 my_int = 2304503000
 my_int = str(my_int)
-print(my_int.count("0"))
+count = my_int.count("0")
+print(count)
 
 ##########################################
 # Задача 2
 
-my_int = 230450300
+my_int = 23045030000
 count = 0
 while True:
     if my_int % 10 == 0:
         count += 1
         my_int = my_int // 10
-        if my_int % 10 > 0:
-            break
+    else:
+        break
 print(count)
 #############################################
 # Задача 3
-
-my_list_1 = [2, 4, 6, 8, 10]
-my_list_2 = [2, 4, 6, 8, 10]
+my_list_1 = [2, 5, 7, 8, 10]
+my_list_2 = [2, 3, 7, 8, 11]
 my_result = []
-for index, my_val_1 in enumerate(my_list_1):
-    if index % 2 == 0:
+for my_val_1 in my_list_1:
+    if my_val_1 % 2 == 0:
         my_result.append(my_val_1)
-for index, my_val_2 in enumerate(my_list_2):
-    if index % 2 != 0:
+for my_val_2 in my_list_2:
+    if my_val_2 % 2 != 0:
         my_result.append(my_val_2)
 print(my_result)
+
+
 #############################################
 # Задача 4
 my_list = [1, 2, 3, 4]
@@ -44,8 +46,8 @@ my_list.insert(len(my_list), my_list.pop(0))
 print(my_list)
 #############################################################
 # Задача 6
-my_str = "43 больше чем 34 и больше чем 39"
-my_str = my_str.split()
+my_str = "43 больше чем 34, но меньше чем 56"
+my_str = my_str.replace(",", "").split()
 my_list = []
 for value in my_str:
     if value.isdigit():
@@ -53,13 +55,19 @@ for value in my_str:
 print(sum(my_list))
 #############################################################
 #Задача 7
-
-my_str = "abcd"
-n = 2
+my_str = "abc"
 my_list = []
-for a in range(0, len(my_str), n):
-    my_list.append(my_str[a:a+n])
+n = 2
+if len(my_str) % 2 == 0:
+    for a in range(0, len(my_str), n):
+        my_list.append(my_str[a:a+n])
+else:
+    if len(my_str) % 2 != 0:
+        my_str = my_str.ljust(len(my_str) + 1, '_')
+    for a in range(0, len(my_str), n):
+        my_list.append(my_str[a:a+n])
 print(my_list)
+
 ###########################################
 # Задача 8
 my_str = "My long string"
