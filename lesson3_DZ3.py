c46884c9403cecca721078fc497bcb2c2834a2ff
Admin <aleksandrbelokrys@gmail.com>
2020-09-16 11:36:41 +0300

    lesson3_DZ3.py.py

--- lesson3_DZ3.py
+++ lesson3_DZ3.py
@@ -5,17 +5,12 @@ count = my_str.count(my_symbol)
 print((my_symbol + "\n") * count)
 
 
-
-
-
-
-
 # Задание 2
-# my_str = "blablacar"
-# my_symbol = "bla"
-# print(my_str.count(my_symbol))
+my_str = "blablacar"
+my_symbol = "bla"
+print(my_str.count(my_symbol))
 ##################################################
 
 # Задание 3
-# my_str="bla BLA car"
-# print(len(set(my_str.lower())))
+my_str = "bla BLA car"
+print(len(set(my_str.lower())))
